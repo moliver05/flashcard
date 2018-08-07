f9a10568571c821c7db7c2aa83a11323210dc508
Julius Bade and Mark Mangahas <mark@>
2018-08-07 16:38:03 -0700

    finished

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,67 @@
 $(document).ready(function() {
-  $("button.Javascript").click(function() {
-      $("#javaoutput").fadeToggle();
+  $("button.click1").click(function() {
+      $("#java").fadeToggle();
 
    });
 
-// $(document).ready(function() {
-  $("button.Operators").click(function() {
+  $("button.click2").click(function() {
       $("#ops").fadeToggle();
 
    });
+
+
+  $("button.click3").click(function() {
+    $("#Var").fadeToggle();
+
+ });
+
+ $("button.click4").click(function() {
+   $("#VarNam").fadeToggle();
+
+});  $("button.click5").click(function() {
+    $("#Fu").fadeToggle();
+
+ });  $("button.click6").click(function() {
+     $("#Meth").fadeToggle();
+
+  });  $("button.click7").click(function() {
+      $("#Arg").fadeToggle();
+
+   });  $("button.click8").click(function() {
+       $("#Par").fadeToggle();
+
+    });  $("button.click9").click(function() {
+        $("#Ret").fadeToggle();
+
+     });  $("button.click10").click(function() {
+         $("#Chain").fadeToggle();
+
+      });  $("button.click11").click(function() {
+          $("#Str").fadeToggle();
+
+       });  $("button.click12").click(function() {
+           $("#boo").fadeToggle();
+
+        });  $("button.click13").click(function() {
+            $("#Und").fadeToggle();
+
+         });  $("button.click14").click(function() {
+             $("#NaN").fadeToggle();
+
+          });  $("button.click15").click(function() {
+              $("#esc").fadeToggle();
+
+           });  $("button.click16").click(function() {
+               $("#Als").fadeToggle();
+
+             });  $("button.click17").click(function() {
+                 $("#Com").fadeToggle();
+
+               });  $("button.click18").click(function() {
+                   $("#JQ").fadeToggle();
+
+                 });  $("button.click19").click(function() {
+                     $("#Att").fadeToggle();
+
+            });
 });
