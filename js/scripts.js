798307270f984677423038c2f8fc3b9a227ca198
Julius Bade and Mark Mangahas <julius@>
2018-08-07 15:55:34 -0700

    added jquery effects with buttons

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $("button.Javascript").click(function() {
+      $("#javaoutput").fadeToggle();
+
+   });
+
+// $(document).ready(function() {
+  $("button.Operators").click(function() {
+      $("#ops").fadeToggle();
+
+   });
+});
